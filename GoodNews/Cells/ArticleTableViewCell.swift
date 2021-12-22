2034358a77392fbb8bd121444c8583308f1a0d99
Mariana <mariana@fastmail.net>
2021-12-22 19:30:30 +0100

    creating cell, displaying news

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Mariana Steblii on 22/12/2021.
+//
+// MARK: - Private properties
+// MARK: - Public properties
+// MARK: - View functions
+// MARK: - Private functions
+// MARK: - Public functions
+// MARK: - @objc private functions
+// MARK: - IBActions
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
